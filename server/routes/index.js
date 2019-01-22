17afa09e19270d49ce9402140aa29a5e38db640e
miriamfrank23 <miriamfrank23@gmail.com>
2019-01-21 20:50:48 -0500

    cors enabled

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,12 @@ import Cards from '../controllers/card';
 
  export default (app) => {
 
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
    app.get('/api/cards', Cards.list);
 
    app.get('/api', (req, res) => res.status(200).send({
