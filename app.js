0cb8012e9929e9d2b2da1921ed6e164033806f55
miriamfrank23 <miriamfrank23@gmail.com>
2019-01-21 20:39:02 -0500

    fixing cors

--- app.js
+++ app.js
@@ -16,6 +16,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 routes(app);
 
+app.use(function(req, res, next) {
+ res.header("Access-Control-Allow-Origin", "*");
+ res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+ next();
+});
+
 app.get('*', (req, res) => res.status(200).send({
  message: 'Welcome to the set app',
 }));
